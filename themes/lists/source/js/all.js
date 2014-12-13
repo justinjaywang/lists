51f8a67f39530aef6db5725d8e0b6946149a0042
justinjaywang <justinjaywang@gmail.com>
2014-12-13 15:57:42 -0800

    added script to navigate next and prev with right and left arrow

--- themes/lists/source/js/all.js
+++ themes/lists/source/js/all.js
@@ -1,5 +1,23 @@
 +function ($) {
   'use strict';
 
+  var leftKeyCode = 37;
+  var rightKeyCode = 39;
+
+  $(document).keydown(function(e) {
+    if (e.keyCode == leftKeyCode) {
+      e.preventDefault();
+      var prev = document.getElementById('prev');
+      if (prev) {
+        window.location.href = prev.href;
+      }
+    } else if (e.keyCode == rightKeyCode) {
+      e.preventDefault();
+      var next = document.getElementById('next');
+      if (next) {
+        window.location.href = next.href;
+      }
+    }
+  });
 
 }(jQuery);
